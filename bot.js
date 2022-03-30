480136f414241143889e540e91956a7f6b70c2fd
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-03-30 20:11:09 +0200

    update this bot 😁

--- bot.js
+++ bot.js
@@ -3,10 +3,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bot = new telegraf.Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Welcome ya man'));
+bot.start((ctx) =>
+  ctx.reply(
+    "Welcome i'm your lovely bot! created by I am a bot created by @mahmoud_alnakeeb"
+  )
+);
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.on('photo' , ctx => ctx.reply('ok'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears('Hi', (ctx) => ctx.reply('Hey there'));
+bot.on('photo', (ctx) => ctx.reply('ok'));
+
+bot.hears(/hi/, (ctx) => ctx.reply('Hey there🙋‍♂️'));
+bot.hears(/Hi/, (ctx) => ctx.reply('Hey there🙋‍♂️'));
+bot.hears(/😂/i, (ctx) => ctx.reply('So funny🤣'));
+bot.hears(/😢/i, (ctx) => ctx.reply('why are you sad😥'));
+bot.hears(/😭/i, (ctx) => ctx.reply("Don't cry i'm here for you😭"));
+bot.hears(/😍/i, (ctx) => ctx.reply('I love you too😍'));
+bot.hears(/😘/i, (ctx) => ctx.reply('I love you too😘'));
+bot.command('about', (ctx) =>
+  ctx.reply('I am a bot created by @mahmoud_alnakeeb')
+);
+
 bot.launch();
