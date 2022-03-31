d740b84ec8816ac2ab13cf982df754c17b4bfcac
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-03-31 04:09:52 +0200

    update

--- bot.js
+++ bot.js
@@ -6,11 +6,28 @@ const bot = new telegraf.Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) =>
   ctx.reply("Welcome i'm your lovely bot created by @mahmoud_alnakeeb")
 );
-
+// just for fun
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears(/hi/, (ctx) => ctx.reply('Hey there🙋‍♂️'));
-bot.hears(/Hi/, (ctx) => ctx.reply('Hey there🙋‍♂️'));
+bot.hears(/اهلا/, (ctx) => ctx.reply(`نورت يا ${ctx.from.first_name}`));
+bot.hears(/أهلا/, (ctx) => ctx.reply(`نورت يا ${ctx.from.first_name}`));
+bot.hears(/😂/, (ctx) => ctx.reply('مضحك جدا😂'));
+bot.hears(/😭/, (ctx) => ctx.reply('محزن😭'));
+bot.hears(/😍/, (ctx) => ctx.reply('كيوت😍'));
+bot.hears(/😘/, (ctx) => ctx.reply('بحبك😘'));
+bot.hears(/😅/, (ctx) => ctx.reply('حسنا حسنا😅'));
+bot.hears(/🤔/, (ctx) => ctx.reply('مش عارف🤔'));
+bot.hears(/🙂/, (ctx) => ctx.reply('مرحبا🙂'));
+bot.hears(/🙄/, (ctx) => ctx.reply('ماذا اقول🙄'));
+bot.hears(/🤣/, (ctx) => ctx.reply('حلو🤣'));
+bot.hears(/🤗/, (ctx) => ctx.reply('شكرا🤗'));
+bot.hears(/🤓/, (ctx) => ctx.reply('حلو حلو🤓'));
+bot.hears(/🤑/, (ctx) => ctx.reply('فلوووس🤑'));
+bot.hears(/🤐/, (ctx) => ctx.reply('افصل شويه الراديو بيفصل🤐'));
+bot.hears(/😨/, (ctx) => ctx.reply('ينهار ابيض😨'));
+bot.hears(/😱/, (ctx) => ctx.reply('الطم على وشى😱'));
+bot.hears(/😡/, (ctx) => ctx.reply('ايه يا عم مالك😡'));
+bot.hears(/🤬/, (ctx) => ctx.reply('متخلنيش اغلط🤬'));
 
 // bot.hears('word', (ctx) => ctx.reply(' respond to this word '));
 
